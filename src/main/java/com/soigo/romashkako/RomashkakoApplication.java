440f9cfb322d6f1136118d1bb1891b4ef79a788b
soigo <daniil.chibitock@yandex.ru>
2025-01-15 16:36:12 +0300

    feat: add spring dependency

--- src/main/java/com/soigo/romashkako/RomashkakoApplication.java
+++ src/main/java/com/soigo/romashkako/RomashkakoApplication.java
@@ -1,7 +1,13 @@
 package com.soigo.romashkako;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class RomashkakoApplication {
+
     public static void main(String[] args) {
+        SpringApplication.run(RomashkakoApplication.class, args);
     }
+
 }
