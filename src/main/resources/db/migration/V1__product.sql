e2e3be8638f15a582741cdd913ef3ca57d0a4d98
soigo <daniil.chibitock@yandex.ru>
2025-01-17 16:17:05 +0300

    fix: price

--- src/main/resources/db/migration/V1__product.sql
+++ src/main/resources/db/migration/V1__product.sql
@@ -1,7 +1,7 @@
 create table product
 (
     id           BIGSERIAL PRIMARY KEY,
-    price        NUMERIC(38, 2) NOT NULL                                                      DEFAULT 0.00,
+    price        NUMERIC(38, 2) NOT NULL                                                      DEFAULT 0.00 CHECK ( price >= 0.00 ),
     name         VARCHAR(255)   NOT NULL,
     description  VARCHAR(4096),
     availability VARCHAR(255)   NOT NULL CHECK (availability IN ('IN_STOCK', 'OUT_OF_STOCK')) DEFAULT 'OUT_OF_STOCK'
