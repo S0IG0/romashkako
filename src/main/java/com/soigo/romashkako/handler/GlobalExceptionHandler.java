c012dd80925a37c279d4373f2bb5aab57ef3a62f
soigo <daniil.chibitock@yandex.ru>
2025-01-17 12:12:11 +0300

    feat: add exception handle

--- src/main/java/com/soigo/romashkako/handler/GlobalExceptionHandler.java
+++ src/main/java/com/soigo/romashkako/handler/GlobalExceptionHandler.java
@@ -5,10 +5,13 @@ import com.soigo.romashkako.exception.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -43,6 +46,36 @@ public class GlobalExceptionHandler {
                         .builder()
                         .uri(request.getRequestURI())
                         .message(ex.getMessage())
+                        .details(Map.of())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(
+            HttpServletRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ErrorResponse
+                        .builder()
+                        .uri(request.getRequestURI())
+                        .message("Ошибка парсинга JSON. Пожалуйста, проверьте формат данных.")
+                        .details(Map.of())
+                        .build()
+        );
+    }
+
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupported(
+            HttpMediaTypeNotSupportedException ex,
+            HttpServletRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(
+                ErrorResponse
+                        .builder()
+                        .uri(request.getRequestURI())
+                        .message("Неподдерживаемый тип медиа: " + ex.getContentType())
                         .details(Map.of())
                         .build()
         );
